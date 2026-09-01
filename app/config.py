7d97119eab4851e1f1e080de836e14cfb2d3c42b
HadesCodex <amazing.aadi04@gmail.com>
2026-09-01 20:12:30 +0530

    Initial Commit
    completed Ingestion pipeline now moving to retrieval pipeline

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settngs:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
+    GROQ_MODEL = "llama-3.3-70b-versatile"
+
+    PORTKEY_API_KEY = os.getenv("PORTKEY_API_KEY")
+
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_COLLECTION = "enterprise_rag"
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+settings = Settngs()
